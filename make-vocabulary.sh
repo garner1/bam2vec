8e4e03ba4c14dd4aa5e02ae454072458dc1966de
garner1 <silvano.garnerone@gmail.com>
2017-04-10 09:16:33 +0200

    parallelized with respect to chr

--- make-vocabulary.sh
+++ make-vocabulary.sh
@@ -3,21 +3,31 @@
 datadir=~/Work/dataset
 samfile=~/Work/dataset/bliss/RM79_BICRO21/outdata_GTCGTATC/RM79_BICRO21.sam
 thr=30				# threshodl on mapping quality 
-len=15				# words' length
+len=70				# words' length
 
+######################################
 # use the bedopts tools to generate a bed file from a sam file
-# sam2bed < $samfile | awk -v threshold=$thr '$5 >= threshold'| cut -f-6,12 > $datadir/bedfile.bed 
+######################################
+# sam2bed < $samfile | awk -v threshold=$thr '$5 >= threshold'| cut -f-3,6,12 > $datadir/bedfile_1.bed 
+cp $datadir/bedfile_1.bed $datadir/bedfile_2.bed 
 
-# filter on quality?
+# cd $datadir
+# rm -f x*
+# split -C 10m $datadir/bedfile.bed 
+# cd -
 
-# head $datadir/bedfile.bed > testfile.txt
+# #REMEMBER to set the len var in the code!!!!
+# parallel 'bash writevoc.sh {} {}.out' ::: `ls $datadir/x*`
+# cat $datadir/*.out > $datadir/vocabulary.bed
 
-cd $datadir
-rm -f x*
-split -C 10m $datadir/bedfile.bed 
-cd -
+# rm $datadir/x*			# clean the data directory
 
-parallel 'bash writevoc.sh {} {}.out' ::: `ls $datadir/x*`
-cat $datadir/*.out > $datadir/vocabulary.bed
+# ######################################
+# # Split vocabulary.bed per chromosome
+# ######################################
+# cd $datadir
+# awk -F"\t" '{print > "vocabulary_"$1".bed"}' < $datadir/vocabulary.bed 
+# cd -
+# rm $datadir/vocabulary.bed 
 
 
