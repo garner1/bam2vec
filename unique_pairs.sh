0a32de32f95bfb43dd38ac88697b012628a88853
garner1 <silvano.garnerone@gmail.com>
2017-04-11 11:06:19 +0200

    unique_pairs.sh optimized

--- unique_pairs.sh
+++ unique_pairs.sh
@@ -4,10 +4,8 @@
 
 file=$1				# input file of pairs
 
-lines=$(cat $file|wc -l)
-for row in $(seq 2 $lines); do
-    tail -n +"$row" $file | awk '{OFS="\t"; print $5,$6,$7,$8,$1,$2,$3,$4}' > "$file"_newfile # FLIP THE BOTTOM FILE
-    head -n "$(($row-1))" $file | cat - "$file"_newfile | # MERGE TOP AND BOTTOM
-    LC_ALL=C sort -u - -o "$file"_newfile		  # REMOVE DUPLICATE PAIRS
-    mv "$file"_newfile $file				  # RENAME THE FILE
-done
+cat -n $file | awk '{print $1"\t"$2"_"$3"_"$4"_"$5"_"$6"_"$7"_"$8"_"$9"_"$10"_"$11}' | LC_ALL=C sort -k2,2 > "$file"_1
+cat -n $file | awk '{print $1"\t"$7"_"$8"_"$9"_"$10"_"$11"_"$2"_"$3"_"$4"_"$5"_"$6}' | LC_ALL=C sort -k2,2 > "$file"_2
+LC_ALL=C join -1 2 -2 2 "$file"_1 "$file"_2 | awk '$2 <= $3' | cut -d' ' -f1 | tr '_' '\t' | LC_ALL=C sort -u > "$file"
+rm -f "$file"_{1,2}
+echo "Done with " $file
